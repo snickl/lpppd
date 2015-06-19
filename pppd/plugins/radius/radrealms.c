99948ff4fa62c95010c45a0a428c27a02a540f30
RobertoM1 <roberto.martelloni@verifone.com>
2015-06-19 14:51:01 +0100

    Fix buffer overflow in radius plugin
    [plugins/radius/clientid.c]: in rc_map2id: warning: call to __builtin___strncat_chk might overflow destination buffer
    [plugins/radius/config.c:156]: (error) Memory leak: iptr
    [plugins/radius/config.c:168]: (error) Memory leak: iptr
    [plugins/radius/radrealms.c:71]: (error) Memory leak: accts
    [plugins/radius/radrealms.c:71]: (error) Memory leak: auths
    
    Signed-off-by: Roberto Martelloni <boos@core-dumped.info>

--- pppd/plugins/radius/radrealms.c
+++ pppd/plugins/radius/radrealms.c
@@ -41,7 +41,7 @@ lookup_realm(char const *user,
 {
     char *realm;
     FILE *fd;
-    SERVER *accts, *auths, *s;
+    SERVER *accts = NULL, *auths = NULL, *s = NULL;
     char buffer[512], *p;
     int line = 0;
     
@@ -65,6 +65,10 @@ lookup_realm(char const *user,
     
     if ((fd = fopen(radrealms_config, "r")) == NULL) {
 	option_error("cannot open %s", radrealms_config);
+	if (auths) 
+		free(auths);
+	if (accts)
+		free(accts);
 	return;
     } 
     info("Reading %s", radrealms_config);
