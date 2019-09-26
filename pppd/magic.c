db436a32532737224e1b649837cb4f63cd0c6ed6
Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
2019-09-26 09:20:58 +0200

    magic: Remove K&R style of arguments
    The __P() macro does not exist in libmusl so
    I switched magic.{c,h} to using the std-c argument style, which had
    already been used in some functions.
    
    Signed-off-by: Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- pppd/magic.c
+++ pppd/magic.c
@@ -60,7 +60,7 @@ extern void srand48(long);
  * and current time, currently.
  */
 void
-magic_init()
+magic_init(void)
 {
     long seed;
     struct timeval t;
@@ -74,7 +74,7 @@ magic_init()
  * magic - Returns the next magic number.
  */
 u_int32_t
-magic()
+magic(void)
 {
     return (u_int32_t) mrand48();
 }
@@ -98,20 +98,19 @@ random_bytes(unsigned char *buf, int len)
  */
 
 double
-drand48()
+drand48(void)
 {
     return (double)random() / (double)0x7fffffffL; /* 2**31-1 */
 }
 
 long
-mrand48()
+mrand48(void)
 {
     return random();
 }
 
 void
-srand48(seedval)
-long seedval;
+srand48(long seedval)
 {
     srandom((int)seedval);
 }
